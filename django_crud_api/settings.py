54a0da08fe4b5150c680d3b9421ef35582a285f0
Néstor <david.camejo@gmail.com>
2023-11-01 22:06:19 -0400

    ajuste settings

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'django_crud_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default="sqlite://db.sqlite3")
+    "default": dj_database_url.config(default="sqlite://db.sqlite3")
 }
 
 
